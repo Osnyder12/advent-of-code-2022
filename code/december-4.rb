66a774fe1654367bbaa76f0f0b65a0df8e824693
Osnyder12 <snyderowen12@gmail.com>
2022-12-06 16:17:52 -0500

    finished dec 4 part 2

--- code/december-4.rb
+++ code/december-4.rb
@@ -13,4 +13,23 @@ final = File.open('data/december-4.txt').map do |line|
   end
 end
 
-puts final.compact.sum
+puts "Part one Answer: #{final.compact.sum}"
+
+# ==========
+# Part 2
+# ==========
+
+final = File.open('data/december-4.txt').map do |line|
+  line.strip!
+  
+  shifts = line.split(',')
+
+  one = shifts[0].split('-')
+  two = shifts[1].split('-')
+
+  if one[0].to_i <= two[1].to_i && two[0].to_i <= one[1].to_i
+    1
+  end
+end
+
+puts "Part two Answer: #{final.compact.sum}"
