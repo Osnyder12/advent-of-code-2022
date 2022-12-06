63ef905cedd6e7f356d9dd2f4f922b162fe6a21c
Osnyder12 <snyderowen12@gmail.com>
2022-12-06 15:05:41 -0500

    part 2 complete for december-2

--- code/december-2.rb
+++ code/december-2.rb
@@ -1,5 +1,7 @@
+require 'pry'
 OPPONENT_WINS = ['A Z', 'B X', 'C Y']
 I_WIN = ['C X', 'A Y', 'B Z']
+TIE = ['A X', 'B Y', 'C Z']
 
 SCORE_FOR_SHOOT = {
   X: 1,
@@ -7,8 +9,7 @@ SCORE_FOR_SHOOT = {
   Z: 3
 }
 
-final = File.open('data/december-2.txt').map do |line|
-  line.strip!
+final = File.open('data/december-2.txt', chomp: true).map do |line|
   hand = line.split(' ').last
   score = SCORE_FOR_SHOOT[hand.to_sym]
 
@@ -21,4 +22,50 @@ final = File.open('data/december-2.txt').map do |line|
   end
 end
 
-puts final.sum
+puts "Part one Answer: #{final.sum}"
+
+# ==========
+# Part 2
+# ==========
+
+OUTCOME = {
+  X: 0,
+  Y: 3,
+  Z: 6
+}
+
+A = {
+  X: ['Z', 3],
+  Y: ['X', 1],
+  Z: ['Y', 2]
+}
+
+B = {
+  X: ['X', 1],
+  Y: ['Y', 2],
+  Z: ['Z', 3]
+}
+
+C = {
+  X: ['Y', 2],
+  Y: ['Z', 3],
+  Z: ['X', 1]
+}
+
+final = File.open('data/december-2.txt', chomp: true).map do |line|
+  outcome = line.split(' ').last
+  opponent_throw = line.split(' ').first
+
+  score = OUTCOME[outcome.to_sym]
+
+  case opponent_throw
+  when 'A'
+    score += A[outcome.to_sym].last
+  when 'B'
+    score += B[outcome.to_sym].last
+  when 'C'
+    score += C[outcome.to_sym].last
+  end
+end
+
+puts "Part two Answer: #{final.sum}"
