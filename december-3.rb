d583ab7c9cef67d8ad2dd1c31f0e151b0ea7d987
Osnyder12 <snyderowen12@gmail.com>
2022-12-06 07:42:23 -0500

    december-3 done

---
+++ december-3.rb
@@ -0,0 +1,16 @@
+priority = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+final = File.open('december-3.txt').map do |line|
+  compartments = line.chars.each_slice(line.length/2).map(&:join)
+
+  c1 = compartments[0].split('').uniq
+  c2 = compartments[1].split('').uniq
+
+  c3 = c1.concat(c2)
+
+  wrong_value = c3.detect{ |e| c3.count(e) > 1 }
+
+  priority.index(wrong_value) + 1
+end
+
+puts final.sum
