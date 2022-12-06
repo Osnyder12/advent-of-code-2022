7cf9817d93716e0cf22d0c7d48c0e9181a5506e8
Osnyder12 <snyderowen12@gmail.com>
2022-12-06 08:09:15 -0500

    december-4

---
+++ december-4.rb
@@ -0,0 +1,13 @@
+final = File.open('december-4.txt').map do |line|
+  line.strip!
+  
+  shifts = line.split(',')
+
+  one = shifts[0].split('-')
+  two = shifts[1].split('-')
+
+  next unless (one[0]..one[1]).cover?(two[0]..two[1]) || (two[0]..two[1]).cover?(one[0]..one[1])
+  1
+end
+
+puts final.compact.sum
