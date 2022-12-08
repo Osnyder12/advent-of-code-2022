3cec735b9ddae4d287d94875a46d6585196183b5
Osnyder12 <snyderowen12@gmail.com>
2022-12-08 12:26:23 -0500

    dec 8 work

--- code/december-8.rb
+++ code/december-8.rb
@@ -1,12 +1,21 @@
-#  grab 3 rows start with the 2nd number and check the row before it with the same index
-#  and after and the 2 numbers left and right 
-#  if all are shorter, += 1
+lines = File.readlines('data/december-8.txt', chomp: true)
+row_start = 0
+row_end = 3
+tree_counter = 0
+while row_end - 3 <= lines.count do
+  row1, row2, row3 = lines[row_start..row_end]
+  row1 = row1.split('').map(&:to_i)
+  row2 = row2.split('').map(&:to_i)
+  row3 = row3.split('').map(&:to_i)
 
-require 'pry'
+  row2.each_with_index do |n, index|
+    next if index == 0 || index == row2.count - 1
+    next if row2[index-1] < n || row2[index+1] < n
+    next if row1[index] < n || row3[index] < n
 
-first_three = File.readlines('data/december-8.txt', chomp: true).each_slice(3)
-
-File.readlines('data/december-8.txt', chomp: true).each_slice(3) do |line|
-  
-binding.pry
+    tree_counter += 1
+  end
+  row_end += 1
 end
+
+puts "Problem 1 solution: #{tree_counter}."
